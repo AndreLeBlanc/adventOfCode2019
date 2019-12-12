22a83de97e720ce382a0c3926635c5ad1ae0051d
AndreLeBlanc <leblanc_112@hotmail.com>
2019-12-12 14:13:26 +0100

    fixed problem 2

--- day1.js
+++ day1.js
@@ -4,10 +4,16 @@ function modWeights () {
       .filter(Boolean);
  }
 
+function cumulative(weight) {
+  const fuel = Math.floor(weight/3 - 2);
+  return (fuel > 0) ? fuel + cumulative(fuel) : 0;
+}
+
 function day1() {
   const modules = modWeights();
-  const ans = modules.map(x => Math.floor(x/3 - 2)).reduce((a, b) => a + b, 0);
-  console.log(ans);
+  const ansPart1 = modules.map(x => Math.floor(x/3 - 2)).reduce((a, b) => a + b, 0);
+  const ansPart2 = modules.map(x => cumulative(x)).reduce((a, b) => a + b, 0);
+  console.log("answer part 1:", ansPart1, "answer part 2:", ansPart2);
 }
 
 day1();
--- day2.js
+++ day2.js
@@ -1,18 +1,52 @@
-function modWeights () {
-  return require('fs').readFileSync('txtfiles/day1.txt', 'utf-8')
-    .split('\n')
-    .filter(Boolean);
+function readr () {
+  return require('fs').readFileSync('txtfiles/day2.txt', 'utf-8')
+    .split(',')
+    .filter(Boolean)
+    .map(Number);
 }
 
-function cumulative(weight) {
-  const fuel = Math.floor(weight/3 - 2);
-  return (fuel > 0) ? fuel + cumulative(fuel) : 0;
+function run(opcodes) {
+  for(let i = 0; i < opcodes.length; i += 4) {
+    switch(opcodes[i]) {
+    case 99:
+      return opcodes;
+    case 1:
+      opcodes[opcodes[i+3]] = opcodes[opcodes[i+1]] + opcodes[opcodes[i+2]];
+      break;
+    default:
+      opcodes[opcodes[i+3]] = opcodes[opcodes[i+1]] * opcodes[opcodes[i+2]];
+    }
+  }
+  return opcodes
+}
+
+function part2(opcodes) {
+   for(let noun = 0; noun < 100; noun++) {
+    for(let verb = 0; verb < 100; verb++) {
+      var program = opcodes.slice();
+      program[1] = noun;
+      program[2] = verb
+      program = run(program);
+      if (program[0] === 19690720) {
+        return 100*noun + verb;
+      }
+    }
+  }
+}
+
+function part1(opcode) {
+  opcode[1] = 12;
+  opcode[2] = 2;
+  run(opcode);
+  return(opcode[0]);
 }
 
 function day2() {
-  const modules = modWeights();
-  const ans = modules.map(x => cumulative(x)).reduce((a, b) => a + b, 0);
-  console.log(ans);
+  var opcodes = readr();
+  const ansPart1 = part1(opcodes.slice());
+  console.log("answer part1:", ansPart1);
+  const ansPart2 = part2(opcodes);
+  console.log("answer part2:", ansPart2);
 }
 
 day2();
