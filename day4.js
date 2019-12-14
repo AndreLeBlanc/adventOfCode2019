6c60e49d52e5d163903115e9e39dd0259185a3fa
AndreLeBlanc <leblanc_112@hotmail.com>
2019-12-14 17:13:30 +0100

    day 4 part 2

--- day4.js
+++ day4.js
@@ -10,10 +10,22 @@ function isPass(x) {
  return duplicates;
 }
 
+function twoAdjacent(x) {
+  const digits = Array.from(x.toString()).map(Number);
+  for (let i = 0; i < 5; i++) {
+    if(digits[i] === digits[i+1] && digits[i] !== digits[i+2]) return true;
+    else {
+      while(digits[i] === digits[i+1] && i < 4) i++;
+    }
+  }
+  return false;
+}
+
 function day4() {
   const potential = Array.from(new Array(471207), (x,i) => i+235741);
-  const ans = potential.filter(x => isPass(x)).length;
-  console.log(ans);
+  const ansPart1 = potential.filter(x => isPass(x));
+  const ansPart2 = ansPart1.filter(x => twoAdjacent(x)).length;
+  console.log("The answer part 1:", ansPart1.length, "The answer part2:", ansPart2);
 }
 
 day4();
